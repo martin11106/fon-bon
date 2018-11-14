df7282aac7fff8ba699317b1b6e92bb30a369981
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2018-11-13 20:53:38 -0600

    test

---
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/medicine/MedicineController.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.springframework.samples.petclinic.medicine;
+
+import java.util.Collection;
+import java.util.Map;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author johan
+ */
+@Controller
+class MedicineController {
+    
+    private static final String VIEWS_MEDICINES_CREATE_OR_UPDATE_FORM="medicines/createOrUpdateMedicineForm";
+    private final MedicineRepository medicines;
+    
+    public MedicineController(MedicineRepository clinicService){
+        this.medicines = clinicService;
+    }
+    
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+    
+    @GetMapping("/medicines/new")
+    public String initCreationForm(Map<String, Object> model){
+        Medicine medicine = new Medicine();
+        model.put("medicine", medicine);
+        return VIEWS_MEDICINES_CREATE_OR_UPDATE_FORM;
+    }
+    
+    @PostMapping("/medicines/new")
+    public String processCreationForm(@Valid Medicine medicine, BindingResult result){
+        if (result.hasErrors()) {
+           return VIEWS_MEDICINES_CREATE_OR_UPDATE_FORM;
+        }else{
+            this.medicines.save(medicine);
+            return "redicrect:/medicines/" + medicine.getId();
+        }
+    }
+    
+    @GetMapping("/medicines/find")
+    public String initFindForm(Map<String, Object> model){
+        model.put("medicine", new Medicine());
+        return "medicines/findMedicines";
+    }
+    
+    @GetMapping("/medicines")
+    public String processFindForm(Medicine medicine, BindingResult result, Map<String, Object> model){
+        if (medicine.getName()==null) {
+            medicine.setName("");
+        }
+        
+        Collection<Medicine> results = this.medicines.findByName(medicine.getName());
+        if (results.isEmpty()) {
+            result.rejectValue("name", "notFound", "not found");
+            return "medicines/findMedicines";
+        }else if(results.size() == 1){
+            medicine = results.iterator().next();
+            return "redirect:/medicines/" + medicine.getId();
+        }else{
+            model.put("selectiones", results);
+            return "medicines/medicinesList";
+        }
+    }
+    
+    @GetMapping("/medicines/{medicineId}/edit")
+    public String initUpdateMedicineForm(@PathVariable("medicineId") int medicineId, Model model){
+        Medicine medicine = this.medicines.findById(medicineId);
+        model.addAttribute(medicine);
+        return VIEWS_MEDICINES_CREATE_OR_UPDATE_FORM;
+    }
+    
+    @PostMapping("/medicines/{medicineId}/edit")
+    public String processUpdateMedicineForm(@Valid Medicine medicine, BindingResult result, @PathVariable("medicineId") int medicineId){
+        if (result.hasErrors()) {
+            return VIEWS_MEDICINES_CREATE_OR_UPDATE_FORM;
+        }else{
+            medicine.setId(medicineId);
+            this.medicines.save(medicine);
+            return "redirect:/medicines/{medicineId}";
+        }
+    }
+    
+    @GetMapping("medicines/{medicineId}")
+    public ModelAndView showMedicine(@PathVariable("medicineId") int medicineId){
+        ModelAndView mav = new ModelAndView("medicines/medicineDetails");
+        mav.addObject(this.medicines.findById(medicineId));
+        return mav;
+    }
+}
---
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/medicine/MedicineRepository.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.springframework.samples.petclinic.medicine;
+
+import java.util.Collection;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author johan
+ */
+public interface MedicineRepository extends Repository<Medicine, Integer> {
+    
+    @Query("SELECT DISTINCT medicine FROM Medicine medicine left join fetch medicine WHERE medicine.name LIKE :name%")
+    @Transactional(readOnly = true)
+    Collection<Medicine> findByName(@Param("name") String name);
+    
+    @Query("SELECT medicine FROM Medicine medicine left join fetch medicine WHERE medicine.id =:id")
+    @Transactional(readOnly = true)
+    Medicine findById(@Param("id") Integer id);
+    
+    void save(Medicine medicine);
+    
+}
