d8809ae6bced6ba2ed42c5d2629e2cbc0f02660d
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2018-11-16 13:41:03 -0600

    added medicine data

--- spring-petclinic-master/src/main/resources/db/mysql/data.sql
+++ spring-petclinic-master/src/main/resources/db/mysql/data.sql
@@ -51,3 +51,8 @@ INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
 INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'spayed');
+
+INSERT IGNORE INTO medicines VALUES (1, 'Paracetamol', 'Pseudoefedrina', 'Capsulas');
+INSERT IGNORE INTO medicines VALUES (2, 'Paracetamol', 'N-acetilcisteína', 'Capsulas');
+INSERT IGNORE INTO medicines VALUES (3, 'Amoxicilina', 'Amoxicilina trihidrato', 'Capsulas');
+INSERT IGNORE INTO medicines VALUES (4, 'Amoxicilina', 'Amoxicilina trihidrato', 'Jarabe');
